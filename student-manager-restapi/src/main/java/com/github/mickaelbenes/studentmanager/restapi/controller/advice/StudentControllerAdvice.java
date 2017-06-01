f4ead497bed9d37bea6843af86d78fcf8465305d
Mickael Benes <mbenes@wideagency.com>
2017-06-01 08:59:22 +0200

    Forgot the @ControllerAdvice annotation.

--- student-manager-restapi/src/main/java/com/github/mickaelbenes/studentmanager/restapi/controller/advice/StudentControllerAdvice.java
+++ student-manager-restapi/src/main/java/com/github/mickaelbenes/studentmanager/restapi/controller/advice/StudentControllerAdvice.java
@@ -2,18 +2,20 @@ package com.github.mickaelbenes.studentmanager.restapi.controller.advice;
 
 import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.github.mickaelbenes.studentmanager.restapi.exception.ProfessorNotFoundException;
 
+@ControllerAdvice
 public class StudentControllerAdvice {
 	
 	@ResponseBody
 	@ExceptionHandler( ProfessorNotFoundException.class )
 	@ResponseStatus( HttpStatus.NOT_FOUND )
-	VndErrors userNotFoundExceptionHandler( ProfessorNotFoundException ex ) {
+	VndErrors professorNotFoundExceptionHandler( ProfessorNotFoundException ex ) {
 		return new VndErrors( "error", ex.getMessage() );
 	}
 
